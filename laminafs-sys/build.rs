cd7dee0cf613e889e9fc4996e65844ce6368e934
Brett Lajzer <brett@brettlajzer.com>
2019-01-10 10:02:09 -0500

    Submodule, repathed build.rs, updated gitignore.

--- laminafs-sys/build.rs
+++ laminafs-sys/build.rs
@@ -6,13 +6,13 @@ use bindgen::builder;
 fn main() {
 	cc::Build::new()
 		.cpp(true)
-		.include("../../src/")
-		.file("../../src/FileContext.cpp")
-		.file("../../src/laminaFS_c.cpp")
-		.file("../../src/device/Directory.cpp")
+		.include("vendor/laminaFS/src/")
+		.file("vendor/laminaFS/src/FileContext.cpp")
+		.file("vendor/laminaFS/src/laminaFS_c.cpp")
+		.file("vendor/laminaFS/src/device/Directory.cpp")
 		.compile("laminafs");
 
-	let bindings = builder().header("../../src/laminaFS_c.h").generate().unwrap();
+	let bindings = builder().header("vendor/laminaFS/src/laminaFS_c.h").generate().unwrap();
 		
 	bindings.write_to_file("src/lib.rs");
 }
